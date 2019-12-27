a2eb3201207029f8afa9aa7520e02d89d64fe1a7
natacharome <natacha-rome@hotmail.fr>
2019-12-27 10:16:38 +0100

    Add sorting and filtering

--- Data/IDatingRepository.cs
+++ Data/IDatingRepository.cs
@@ -14,8 +14,6 @@ namespace DatingApp.Data
         Task<bool> SaveAll();
         Task<PagedList<User>> GetUsers(UserParams userParams);
         Task<User> GetUser(int id);
-
-        //Task GetPhoto(int id);
         Task<Photo> GetPhoto(int id);
         Task<Photo> GetMainPhotoForUser(int userId);
     }
