473326cd1abf9aec34e6d12d68497ebd6da78ab6
natacharome <natacha-rome@hotmail.fr>
2019-12-27 11:21:37 +0100

    Implement userLikes methods

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using AutoMapper;
 using DatingApp.Data;
 using DatingApp.Dtos;
 using DatingApp.Helpers;
+using DatingApp.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -76,6 +77,33 @@ namespace DatingApp.Controllers
                 return NoContent();
 
             throw new Exception($"Updating user {id} failed on save");
+        }
+
+        [HttpPost("{id}/like/{recipientId}")]
+        public async Task<IActionResult> LikeUser(int id, int recipientId)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var like = await _repo.GetLike(id, recipientId);
+
+            if (like != null)
+                return BadRequest("You already like the user");
+
+            if (await _repo.GetUser(recipientId) == null)
+                return NotFound();
+            like = new Like
+            {
+                LikerId = id,
+                LikeeId = recipientId
+            };
+
+            _repo.Add<Like>(like);
+
+            if (await _repo.SaveAll())
+                return Ok();
+            return BadRequest("Failed to like user");
+
         }
     }
 }
